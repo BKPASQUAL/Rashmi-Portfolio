cb34023f4d3e541be91de08d995c21894df045f7
Bawantha <bawanthapasqual@gmail.com>
2025-01-22 07:29:29 +0530

    Change cv

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -135,7 +135,7 @@ function Navbar() {
             GitHub
           </a>
           <a
-            href="/RDJayasekara.pdf" 
+            href="/Rashmi_Jayasekara_Resume.pdf" 
             target="_blank"
             rel="noopener noreferrer"
             className="cursor-pointer text-lg font-semibold text-navy hover:text-gray-400 flex items-center"
@@ -164,7 +164,7 @@ function Navbar() {
           GitHub
         </a>
         <a
-        href="/RDJayasekara.pdf" 
+        href="/Rashmi_Jayasekara_Resume.pdf" 
           target="_blank"
           rel="noopener noreferrer"
           className="cursor-pointer text-navy hover:text-navy hover:font-extrabold flex items-center "
